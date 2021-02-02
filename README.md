f34f00c4f9bbb2abbbb9a792b8f83c6f664a9ebf
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2021-02-02 12:59:26 +0000

    Notes

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Notes
+## With python notes you can make simple markdown notes ready to be used
+- Dont forget to create notes dir in the root directory
--- notepad.py
+++ notepad.py
@@ -2,27 +2,55 @@ import os
 
 count = 1
 
+def space():
+        global title
+        if " " in title:
+            print('Your note\'s title can\'t contain whitespaces')
+            title = input('Enter you list\'s name >>> ')
+            space()
+
 title = input('Enter you list\'s name >>> ')
-# item = input(f'{count}. Enter your list item >>> ')
+space()
+item = input(f'{count}. Enter your list item >>> ')
+new_item = ''
 
 def invalid_answer():
     global new_item
+
     if new_item.lower() != 'yes' and new_item.lower() != 'no':
         new_item = input('You need to enter either Yes or No')
         invalid_answer()
+
+if os.path.exists('notes'):
+    def note_name():
+        global title
+        with open(os.path.join("./notes", f'{title}.md'), "a") as f:
+            f.writelines(f'# {title}\r\n')
+    def write():
+            global count, item, title
+            with open(os.path.join("./notes", f'{title}.md'), "a") as f:
+                f.writelines(f'- {item}\r')
+                f.close
+else:
+    print('You need to create a notes directory') 
 
 def list():
     def new():
         global new_item, item, count
         new_item = input('\nNew Item? Yes / No\r\n')
-        item = input(f'{count}. Enter your list item >>> ')
         invalid_answer()
         if new_item.lower() == 'yes':
             count += 1
             item = input(f'{count}. Enter your list item >>> ')
+            write()
+            new()
         elif new_item.lower() == 'no':
-            print('no')
+            file_path = os.path.abspath(f'{title}.md')
+            print(f'Your note has been saved {file_path}')
+            exit()
 
     new()
 
+note_name()
+write()
 list()
